55f0241cbbe34911d004161c21ce51cd3901a24b
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-09 22:06:34 -0300

    Implementing routes for sugestions screen

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,19 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './photo-details.component.html'
+})
+
+export class PhotoDetailsComponent implements OnInit{
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params.photoId;
+    console.log(id);
+
+  }
+
+
+}
