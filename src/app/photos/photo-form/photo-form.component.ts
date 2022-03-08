d40bb932aa452f48095f8a010efefcd392f1d641
Gabriel Simon <gabrielsimon775@gmail.com>
2022-03-08 20:47:34 -0300

    loading bar completed

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'ap-photo-form',
@@ -40,18 +41,19 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
 
     this.photoService.upload(description, allowComments, this.file)
+    .pipe(finalize(() => {
+      this.router.navigate(['/user', this.userService.getUserName()]);
+    }))
     .subscribe((event: HttpEvent<any>) => {
       if(event.type == HttpEventType.UploadProgress) {
         this.percentDone = Math.round(100 * event.loaded / event.total);
       } else if (event.type == HttpEventType.Response) {
         this.alertService.success("Upload complete", true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
       }
     },
     err => {
       console.log(err);
-      this.alertService.danger('Upload error!');
-      this.router.navigate(['/user', this.userService.getUserName()]);
+      this.alertService.danger('Upload error!', true);
     });
   }
 
