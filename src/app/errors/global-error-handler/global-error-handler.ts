88c13625453b828c87fb94e20c5ddc55e1ac3e5a
Gabriel Simon <gabrielsimon775@gmail.com>
2022-03-19 18:09:53 -0300

    Global error handler being implemeting

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,17 +1,31 @@
-import { ErrorHandler } from '@angular/core';
+import { UserService } from './../../core/user/user.service';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
 
+@Injectable()
 
 export class GlobalErrorHandler implements ErrorHandler {
 
+  constructor(private injector: Injector) {
+
+  }
+
   handleError(error: any): void {
     console.log("Passou no handler de error");
-    StackTrace
-    .fromError(error)
+
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
+
+    const message = error.message ? error.message : error.toString();
+
+    StackTrace.fromError(error)
     .then(stackFrames => {
       const stackAsString = stackFrames.map(sf => sf.toString()).join('/n');
+      console.log(message);
       console.log(stackAsString);
-
+      console.log({message, url, userName: userService.getUserName, stack: stackAsString});
 
     })
   }
