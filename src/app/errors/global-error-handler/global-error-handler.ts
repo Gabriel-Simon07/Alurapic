3a12e10901f34c63cbfa20ed48e2146700c7e205
Gabriel Simon <gabrielsimon775@gmail.com>
2022-03-18 23:01:56 -0300

    Added stacktrace-js in project

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,10 +1,19 @@
 import { ErrorHandler } from '@angular/core';
+import * as StackTrace from 'stacktrace-js';
+
 
 export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
     console.log("Passou no handler de error");
-    throw error;
+    StackTrace
+    .fromError(error)
+    .then(stackFrames => {
+      const stackAsString = stackFrames.map(sf => sf.toString()).join('/n');
+      console.log(stackAsString);
+
+
+    })
   }
 
 }
